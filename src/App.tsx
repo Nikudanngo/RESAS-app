2a8db9647442543571a8a4a2df1db0af64f2036e
nikudango <satoru_rutosa@icloud.com>
2022-05-08 01:08:03 +0900

    change directory name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import TestFetch from "./data/testFetch";
+import TestFetch from "./component/testFetch";
 
 function App() {
   return <TestFetch />;
