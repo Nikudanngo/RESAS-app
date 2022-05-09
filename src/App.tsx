913a15c0e953bc358bc60691bbbdafe9bbb9c1a0
Nikudanngo <satoru_rutosa@icloud.com>
2022-05-09 21:32:58 +0900

    index -> App -> Header,Fetch ->  Graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-// import React from "react";
-// import logo from "./logo.svg";
-import "./App.css";
-import TestFetch from "./component/testFetch";
-
+import React from "react";
+import GlobalStyles from "./style/GlobalStyles";
+import { LightTheme, DarkTheme } from "./style/Theme";
+import Header from "./component/Header";
+import Fetch from "./component/Fetch";
 function App() {
-  return <TestFetch />;
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+  return (
+    <div>
+      <GlobalStyles theme={isDarkMode ? DarkTheme.theme : LightTheme.theme} />
+      <Header />
+      <Fetch />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Fetch from "./component/Fetch";
-import styled from "styled-components";
-
-// 即席レスポンス
-const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  max-width: 1228px;
-  margin: 0 auto;
-  background-color: #f8f8f8;
-  @media (max-width: 1227px) {
-    max-width: 645px;
-  }
-  @media (max-width: 644px) {
-    max-width: 432px;
-  }
-  @media (max-width: 431px) {
-    max-width: 361px;
-  }
-  @media (max-width: 360px) {
-    max-width: 291px;
-  }
-`;
-
-// center the header
-const Headers = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50px;
-  background-color: #f8f8f8;
-  border-bottom: 2px solid #e0e0e0;
-  padding: 0 20px;
-  font-size: 20px;
-`;
-// background
-const Background = styled.div`
-  background-color: #e5e5e5;
-`;
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Background>
-      <Main>
-        <Headers>全国の人口増加率</Headers>
-        <Fetch />
-      </Main>
-    </Background>
+    <App />
+    {/* <TestFetch /> */}
   </React.StrictMode>
 );
 
